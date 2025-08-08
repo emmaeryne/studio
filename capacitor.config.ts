4649a9581225805c4820f13ff2e51894b65061c1
emma emma <emnaawini@gmail.com>
2025-08-08 05:10:12 +0000

    Import trace for requested module: ./node_modules/handlebars/lib/index.j

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,9 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.liaisonlegale.app',
   appName: 'Liaison Légale',
-  webDir: 'out',
+  webDir: '.next',
   server: {
-    androidScheme: 'https'
+    androidScheme: 'https',
+    // Si vous déployez votre application sur un serveur, mettez l'URL ici.
+    // Pour les tests locaux, cela n'est pas nécessaire.
+    // url: 'http://192.168.1.10:3000'
   }
 };
 
